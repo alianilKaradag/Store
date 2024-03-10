b8efd4bb2bfc71211118e70edd87bd2d29a2fe96
AnilKaradag <anilkaradag2101@gmail.com>
2024-03-10 18:46:09 +0300

    Initial commit

---
+++ StoreApp/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace StoreApp.Models{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
