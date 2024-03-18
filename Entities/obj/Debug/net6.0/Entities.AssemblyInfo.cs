85a2449c2c31ad8361523c20f60bc4ffb901bbaf
AnilKaradag <anilkaradag2101@gmail.com>
2024-03-18 22:28:34 +0300

    [Feature] Product, Category and Dashboard areas added

--- Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67acc93731402ac8aeb2d754d1259c227fc7817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5562c8d8e0896e4051368403ba7891572d634a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -6,8 +6,9 @@ using Services.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RepositoryContext>(options => {
-    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),  b => b.MigrationsAssembly("StoreApp"));
+builder.Services.AddDbContext<RepositoryContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"), b => b.MigrationsAssembly("StoreApp"));
 });
 
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
@@ -22,8 +23,18 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
-app.MapControllerRoute(
-    name:"default",
-    pattern:"{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name:"Admin",
+        areaName:"Admin",
+        pattern:"Admin/{controller=Dashboard}/{action=Index}/{id?}"
+    );
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
