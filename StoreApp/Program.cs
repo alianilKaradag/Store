6dcbd9b73da8fffe5b3ec7940681b4fa51d0c3aa
AnilKaradag <anilkaradag2101@gmail.com>
2024-04-29 15:03:08 +0300

    [Feature] Identity library and IdentityDbContext added.

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureIdentity();
 builder.Services.ConfigureSession();
 builder.Services.ConfigureRepositoryRegistration();
 builder.Services.ConfigureServiceRegistration();
