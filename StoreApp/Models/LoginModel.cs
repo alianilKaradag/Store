fb920a1f1e96f5df8ddc8fa98e8004f1a4f4a8d3
AnilKaradag <anilkaradag2101@gmail.com>
2024-05-02 21:24:22 +0300

    [Feature] Registration page added.

--- StoreApp/Models/LoginModel.cs
+++ StoreApp/Models/LoginModel.cs
@@ -17,6 +17,7 @@ namespace StoreApp.Models
 
         [Required(ErrorMessage = "Name is required!")]
         public string? Name { get; set; }
+        
         [Required(ErrorMessage = "Password is required!")]
         public string? Password { get; set; }
     }
