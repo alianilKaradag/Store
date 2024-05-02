8a3105a8cffa5826d90633f870547cef39b2f1e0
AnilKaradag <anilkaradag2101@gmail.com>
2024-05-02 22:23:22 +0300

    [Feature] User and role pages added.

---
+++ Services/Contracts/IAuthService.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Services.Contracts
+{
+    public interface IAuthService{
+        public IEnumerable<IdentityRole> Roles { get; }
+        public IEnumerable<IdentityUser> GetAllUsers();
+    }
+}
--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -10,15 +10,19 @@ namespace Services
 
         public IOrderService OrderService => _orderService;
 
+        public IAuthService AuthService => _authService;
+
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
         private readonly IOrderService _orderService;
+        private readonly IAuthService _authService;
 
-        public ServiceManager(ICategoryService categoryService, IProductService productService, IOrderService orderService)
+        public ServiceManager(ICategoryService categoryService, IProductService productService, IOrderService orderService, IAuthService authService)
         {
             _productService = productService;
             _categoryService = categoryService;
             _orderService = orderService;
+            _authService = authService;
         }
     }
 }
